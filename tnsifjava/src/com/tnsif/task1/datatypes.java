29837452ac2cbb5c294ced25bf97127aac1dab02
susha <susha@Deepak.lan>
2026-05-27 12:40:52 +0530

    addition

--- tnsifjava/src/com/tnsif/task1/datatypes.java
+++ tnsifjava/src/com/tnsif/task1/datatypes.java
@@ -4,7 +4,9 @@ public class datatypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+         int a =5;
+         int  b= 9;
+         System.out.println(a+b);
 	}
 
 }
