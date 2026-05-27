56274ec232342e41b2e83bcf8de0926570496c2b
susha <susha@Deepak.lan>
2026-05-27 15:45:29 +0530

    even or odd program

---
+++ tnsif_java/src/tnsif_java/evenorodd.java
@@ -0,0 +1,16 @@
+package tnsif_java;
+
+public class evenorodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		      int a=50;
+		      if ( a%2==0) {
+		    	  System.out.println("it is even number");
+		      }
+		      else { 
+		    	  System.out.println("it is odd number");
+		      }
+	}
+
+}
