520efa8319fd458ce105e5fd9c3e68c79c794136
susha <susha@Deepak.lan>
2026-05-27 15:40:28 +0530

    just

---
+++ tnsif_java/src/tnsif_java/datatypes.java
@@ -0,0 +1,12 @@
+package tnsif_java;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 20;
+        int b=30;
+        System.out.println(a+b);
+	}
+
+}
